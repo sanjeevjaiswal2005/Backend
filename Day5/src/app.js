93ef984f53c6a23e046b27021f10514cb577c7b9
Sanjeev Jaiswal <sanjeevjaiswal2005@gmail.com>
2026-01-25 19:16:33 +0530

    day5

---
+++ Day5/src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const notes = [];
+
+/*
+FOR POST mathod 
+*/
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.send("Notes created sucessfully...");
+});
+
+/*
+For GET methods ==> retrived data from the server
+*/
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
+
+/*
+PATCH==> Update a specifice resources
+*/
+
+app.patch('/notes/:index', (req, res) => {
+    notes[req.params.index].discription = req.body.discription;
+
+    res.send(notes)
+})
+
+/* DELETE resource.... */
+app.delete("/notes/:index", (req, res) => {
+delete notes[req.params.index]
+  res.send("Notes is deleted sucessfully......");
+});
+
+
+module.exports = app;
